148f2c3e2638b2935cad12cd0a423e686e55a614
Paulo Ricardo W. Medeiros <82722771+pauloricardotech@users.noreply.github.com>
2022-05-13 09:56:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 yum install httpd -y
-echo "
-# MariaDB 10.2 CentOS repository list - created 2017-02-21 12:11 UTC
-# http://downloads.mariadb.org/mariadb/repositories/
-[mariadb]
+echo "[mariadb]
 name = MariaDB
 baseurl = http://yum.mariadb.org/10.2/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
-" >> /etc/yum.repos.d/MariaDB.repo
+gpgcheck=1 >> /etc/yum.repos.d/MariaDB.repo
 
 
 yum -y install MariaDB-server MariaDB-client
